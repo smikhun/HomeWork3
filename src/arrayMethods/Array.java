8d2854e9e8cb282fe74a1b1f5ba0484f4adf478c
smikhun <a.smikhun@gmail.com>
2015-11-27 17:11:36 +0200

    fix new vesion

---
+++ src/arrayMethods/Array.java
@@ -0,0 +1,7 @@
+package arrayMethods;
+
+/**
+ * Created by Administrator on 27.11.2015.
+ */
+public class Array {
+}
--- src/circus/TightropeWalker.java
+++ src/circus/TightropeWalker.java
@@ -3,5 +3,9 @@ package circus;
 /**
  * Created by Andriy on 26.11.2015.
  */
-public class TightropeWalker {
+public class TightropeWalker extends Employee{
+    TightropeWalker(String name, String passportdate, double salary){
+        super(name,passportdate,salary);
+
+    }
 }
